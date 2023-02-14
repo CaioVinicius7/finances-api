69667e69fd69cb55f3f9984fcf2fe9266e26aa51
Caio Vinícius <caio1525pereira@gmail.com>
2023-02-13 22:06:04 -0300

    feat: :wrench: Mudança do diretório tmp para db e configuração do diretório padrão para migrations
    Banco de dados - Criando primeira migration

--- src/database.ts
+++ src/database.ts
@@ -3,9 +3,13 @@ import { knex as setupKnex, Knex } from "knex";
 export const config: Knex.Config = {
   client: "sqlite",
   connection: {
-    filename: "./tmp/app.db"
+    filename: "./db/app.db"
   },
-  useNullAsDefault: true
+  useNullAsDefault: true,
+  migrations: {
+    extension: "ts",
+    directory: "./db/migrations"
+  }
 };
 
 export const knex = setupKnex(config);
