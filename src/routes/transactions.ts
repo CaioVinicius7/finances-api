6e1b576327e14838b8b51d196e35ca676a1e4d93
Caio Vinícius <caio1525pereira@gmail.com>
2023-02-16 23:44:07 -0300

    feat: :building_construction: Desacoplando rotas
    Implementando rotas - Plugins do Fastify

---
+++ src/routes/transactions.ts
@@ -0,0 +1,13 @@
+import { FastifyInstance } from "fastify";
+
+import { knex } from "../database";
+
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.get("/", async () => {
+    const transactions = await knex("transactions")
+      .select("*")
+      .where("amount", 1000);
+
+    return transactions;
+  });
+}
