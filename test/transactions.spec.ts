29c4b4e2e3cbdd926cd0f337cffee0c97c70db0b
Caio Vinícius <caio1525pereira@gmail.com>
2023-02-23 18:13:48 -0300

    test: :white_check_mark: Categorizando os testes
    Testes automatizados - Categorizando os testes

---
+++ test/transactions.spec.ts
@@ -0,0 +1,26 @@
+import { beforeAll, afterAll, describe, it, expect } from "vitest";
+import request from "supertest";
+
+import { app } from "../src/app";
+
+describe("Transactions routes", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("Should be able to create a new transaction", async () => {
+    const { statusCode } = await request(app.server)
+      .post("/transactions")
+      .send({
+        title: "New transaction",
+        amount: 5000,
+        type: "credit"
+      });
+
+    expect(statusCode).toEqual(201);
+  });
+});
